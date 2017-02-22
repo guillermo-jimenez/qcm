5800696f4bae0c670482c55a1249c83b83810d7d
Guillermo Jimenez-Perez <guillermo.jimenez@upf.edu>
2017-02-22 19:08:43 +0100

    Added native VTK extraction methods (much faster than MATLAB's version)

--- main.py
+++ main.py
@@ -18,23 +18,50 @@
 
 
 import os;
+import numpy;
 import vtk;
+
 import ConfigParser;
 
 
-Config = ConfigParser.ConfigParser();
+# Config = ConfigParser.ConfigParser();
 
-Config.read("c:\\tomorrow.ini");
-
-
-
-
-for subdir, dirs, files in os.walk(rootdir):
-    for file in files:
+# Config.read("c:\\tomorrow.ini");
 
 
 
 
+# for subdir, dirs, files in os.walk(rootdir):
+#     for file in files:
+
+
+
+
+
+    # try:
+    #     val = int(userInput)
+    # except ValueError:
+    #     print("That's not an int!"
+
+    # try:
+    #     val = int(userInput)
+    # except ValueError:
+    #     print("That's not an int!"
+
+    # try:
+    #     val = int(userInput)
+    # except ValueError:
+    #     print("That's not an int!"
+
+    # try:
+    #     val = int(userInput)
+    # except ValueError:
+    #     print("That's not an int!"
+
+    # try:
+    #     val = int(userInput)
+    # except ValueError:
+    #     print("That's not an int!"
 
 
 
@@ -67,17 +94,112 @@ def docstring_example():
     """
 
 
-def reader_vtk():
-    reader = vtk.vtkXMLPolyDataReader();
-    path = os.path.join(constant.BASE_DIR, "archive.vtp"); #Archive path
+
+# path = os.path.join("/home/guille/BitBucket/qcm/data/pat1/MRI", "pat1_MRI_Layer_6.vtk");
+
+def reader_vtk(path):
+    reader = vtk.vtkPolyDataReader();
     reader.SetFileName(path);
     reader.Update();
-    
-    mapper = vtk.vtkPolyDataMapper();
-    mapper.SetInput(reader.GetOutput());
-    
-    actor = vtk.vtkActor();
-    actor.SetMapper(mapper);
+
+    return reader.GetOutput();
+
+
+
+def extract_polygons(polyData):
+    rows                = None;
+    cols                = None;
+    polygons            = None;
+
+    idlist              = vtk.vtkIdList();
+
+    try:
+        polys = polyData.GetPolys();
+    except RuntimeError:
+        print("Tried to call function 'extract_polygons' with a                \
+               variable with no 'GetPolys()' method. Check input.");
+
+    for i in range(0, polyData.GetPolys().GetNumberOfCells()):
+        success         = polys.GetNextCell(idlist);
+        polygon_tuple   = polyData.GetPoint(idlist.GetId(0));
+
+        if polygons is None:
+            rows        = len(polygon_tuple);
+            cols        = polyData.GetPolys().GetNumberOfCells();
+            polygons    = numpy.zeros((rows,cols));
+        
+        polygons[0,i]   = polygon_tuple[0];
+        polygons[1,i]   = polygon_tuple[1];
+        polygons[2,i]   = polygon_tuple[2];
+
+    return polygons;
+
+
+
+def extract_points(polyData):
+    rows                        = None;
+    cols                        = None;
+    points                      = None;
+
+    try:
+        pointVector             = polyData.GetPoints();
+    except RuntimeError:
+        print("Tried to call function 'extract_points' with a                \
+               variable with no 'GetPoints()' method. Check input.");
+
+    if pointVector:
+        for i in range(0, pointVector.GetNumberOfPoints()):
+            point_tuple         = pointVector.GetPoint(i);
+
+            if points is None:
+                rows            = len(point_tuple);
+                cols            = pointVector.GetNumberOfPoints();
+                points          = numpy.zeros((rows,cols));
+            
+            points[0,i]         = point_tuple[0];
+            points[1,i]         = point_tuple[1];
+            points[2,i]         = point_tuple[2];
+
+    return points;
+
+
+
+def extract_normals(polyData):
+    rows                        = None;
+    cols                        = None;
+    normals                     = None;
+
+    try:
+        normalVector            = polyData.GetPointData().GetNormals();
+    except RuntimeError:
+        print("Tried to call function 'extract_normals' with a                \
+               variable with no 'GetNormals()' method. Check input.");
+
+    if normalVector:
+        for i in range(0, normalVector.GetNumberOfTuples()):
+            normal_tuple        = normalVector.GetTuple(i);
+
+            if normals is None:
+                rows            = len(normal_tuple);
+                cols            = normalVector.GetNumberOfTuples();
+                normals         = numpy.zeros((rows,cols));
+            
+            normals[0,i]        = normal_tuple[0];
+            normals[1,i]        = normal_tuple[1];
+            normals[2,i]        = normal_tuple[2];
+
+    return normals;
+
+
+
+
+
+
+
+
+
+
+
 
 
 
